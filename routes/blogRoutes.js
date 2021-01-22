b81567759fbc1e893bbe8c23962c35467b2e7cbb
Prabdeep Dhaliwal <prab.dhaliwal95@gmail.com>
2021-01-22 17:44:24 -0500

    Add delete and put requests for blog

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,23 +1,27 @@
 const router = require('express').Router();
+const article = require('../models/articleSchema');
 const Article = require('../models/articleSchema');
 
-// Blogs home page
+// get blogs home page
 router.get('/', async (req, res) => {
   const articles = await Article.find().sort({ createdAt: 'desc' });
 
   res.render('index', { title: 'Home', articles });
 });
 
+// get new article page
 router.get('/new', async (req, res) => {
-  res.render('new', { title: 'New Article' });
+  res.render('new', { title: 'New Article', article: new Article() });
 });
 
+// get edit article page
 router.get('/edit/:id', async (req, res) => {
-  const article = await Article.findById({ id: req.params.id });
+  const article = await Article.findById(req.params.id);
 
   res.render('edit', { title: 'Edit Article', article });
 });
 
+// create new article
 router.post('/new', async (req, res) => {
   const { title, author, description, markdown } = req.body;
 
@@ -31,7 +35,33 @@ router.post('/new', async (req, res) => {
   const saveArticle = await article.save((err, result) => {
     if (err) console.log(err);
     else console.log('article successfully saved!' + result);
+    res.redirect('/blogs');
   });
+});
+
+// edit article
+router.put('/edit/:id', async (req, res) => {
+  const { title, author, description, markdown } = req.body;
+
+  const article = await Article.findById(req.params.id);
+
+  article.title = title;
+  article.author = author;
+  article.description = description;
+  article.markdown = markdown;
+
+  const saveArticle = await article.save((err, result) => {
+    if (err) console.log(err);
+    else console.log('article successfully saved!' + result);
+    res.redirect('/blogs');
+  });
+});
+
+// delete article
+router.delete('/:id', async (req, res) => {
+  await Article.findByIdAndDelete(req.params.id);
+
+  res.redirect('/blogs');
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(methodOverride());
+app.use(methodOverride('_method'));
 
 // Routes
 app.get('/', (req, res) => {
